d90359049f89ac162203f53d370b9bdd449b246c
imp2311 <pierfrancesco.cocchi@gmail.com>
2025-06-03 21:46:35 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -117,4 +117,6 @@ def update_graph(symbol):
     return fig
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
+
+
